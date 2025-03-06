4077f3d37cf0a3c31332b77ade6bf7a36130bb37
Kjeff24 <jeffreyarthur123@gmail.com>
2025-03-06 07:20:45 +0000

    Make jwt service a bean

--- src/main/java/com/bexos/real_time_leaderboard/service/impl/JwtServiceImpl.java
+++ src/main/java/com/bexos/real_time_leaderboard/service/impl/JwtServiceImpl.java
@@ -8,12 +8,14 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class JwtServiceImpl implements JwtService {
 
     @Value("${jwt.secret-key}")
